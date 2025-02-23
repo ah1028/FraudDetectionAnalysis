e1a9ff89897985ee9c6ed86d8a0c1b0662668a84
ah1028 <amyhersey04@gmail.com>
2025-02-23 22:54:55 +0000

    Performed some analysis of data

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import sqlite3
 
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
-
-query = '''SELECT * FROM transactions'''
+# Display transaction type against count of fraudulant transactions
+query = '''SELECT Transaction_Type, COUNT(*) AS count FROM transactions WHERE Fraud_Label=1 GROUP BY Transaction_Type'''
 cursor.execute(query)
-
 output = cursor.fetchall()
-
-# Print results
-for row in output:
-    print(row)
+labels, y = zip(*output)
+plt.pie(y, labels=labels)
+plt.show()
 
 conn.commit()
 conn.close()
